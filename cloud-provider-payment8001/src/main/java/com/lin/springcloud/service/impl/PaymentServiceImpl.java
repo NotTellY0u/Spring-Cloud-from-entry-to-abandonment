5cda2ce8242a5037736cfc2e2b934ba250d54436
lin <yangfulin123@qq.com>
2020-05-19 09:17:02 +0800

    payment8001

---
+++ cloud-provider-payment8001/src/main/java/com/lin/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.lin.springcloud.service.impl;
+
+import com.lin.springcloud.dao.PaymentDao;
+import com.lin.springcloud.entities.Payment;
+import com.lin.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    }
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+}
