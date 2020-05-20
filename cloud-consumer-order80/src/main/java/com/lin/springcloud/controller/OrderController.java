9b9b46a680ee792d6b7bef9ccf1431db98d1ccb1
lin <yangfulin123@qq.com>
2020-05-20 09:14:58 +0800

    api-common

---
+++ cloud-consumer-order80/src/main/java/com/lin/springcloud/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.lin.springcloud.controller;
+
+import com.lin.springcloud.entities.CommonResult;
+import com.lin.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        log.info("创建成功！");
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        log.info("查询成功！");
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/lin/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/lin/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.lin.springcloud.entities.CommonResult;
 import com.lin.springcloud.entities.Payment;
 import com.lin.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,13 +16,13 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("******插入结果：" + result);
         if (result > 0) {
             return new CommonResult(200, "插入数据成功", result);
         } else {
-            return new CommonResult(444, "插入数据失败", null);
+            return new CommonResult(404, "插入数据失败", null);
         }
     }
 
