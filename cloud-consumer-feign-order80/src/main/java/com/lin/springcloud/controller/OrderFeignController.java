363887ba7d41a606babd23cbe0489fa2062025b7
yfl <yangfulin123@qq.com>
2020-05-24 22:33:51 +0800

    openfeign

--- cloud-consumer-feign-order80/src/main/java/com/lin/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/lin/springcloud/controller/OrderFeignController.java
@@ -12,12 +12,17 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
-public class OrderFeignController {
+public class  OrderFeignController {
     @Resource
     private PaymentFeignService paymentFeignService;
 
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeOut(){
+        return paymentFeignService.paymentFeignTimeOut();
     }
 }
