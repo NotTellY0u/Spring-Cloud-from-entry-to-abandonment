5f88fb086bd80d2ddfd962c0c0337df3052596ce
yfl <yangfulin123@qq.com>
2020-06-27 16:09:44 +0800

    hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,5 @@
+package com.lin.springcloud;
+
+public class OrderHystrixMain80 {
+
+}
--- cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/PaymentHystrixMain8001.java
@@ -1,4 +1,14 @@
 package com.lin.springcloud;
 
-public class PaymentHystrix {
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
 }
