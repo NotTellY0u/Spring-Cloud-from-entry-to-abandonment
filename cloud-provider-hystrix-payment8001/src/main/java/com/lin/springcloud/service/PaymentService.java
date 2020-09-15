0d5e277ba7b939cdcfbc7bc0470fa46952309e82
lin <yangfulin123@qq.com>
2020-09-15 09:22:49 +0800

    payment

--- cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.lin.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -27,6 +30,15 @@ public class PaymentService {
 
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "线程池" + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler,id" + id + '\t' + "o(╥﹏╥)o";
+    }
+
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if(id<0){
+            throw new RuntimeException("id 不能为负数");
+        }
+        String serialNumble = IdUtil.simpleUUID();
+        UUID.randomUUID().toString();
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号"+serialNumble;
     }
 
 }
