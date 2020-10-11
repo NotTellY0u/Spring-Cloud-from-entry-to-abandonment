c3749f431d9b3c07f62b2b7d0d383687d1ad4b45
yfl <yangfulin123@qq.com>
2020-10-11 14:52:16 +0800

    1

--- cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.lin.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -10,13 +12,19 @@ public class PaymentService {
         return "线程池" + Thread.currentThread().getName() + "paymentinfo_OK,id" + id;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id) {
-        int timenumber = 3;
+        int timenumber = 5;
         try {
             TimeUnit.SECONDS.sleep(timenumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池" + Thread.currentThread().getName() + "paymentinfo_OUT,id" + id;
+    }
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池" + Thread.currentThread().getName() + "paymentinfo_timeOUTHandler,id" + id;
     }
 }
