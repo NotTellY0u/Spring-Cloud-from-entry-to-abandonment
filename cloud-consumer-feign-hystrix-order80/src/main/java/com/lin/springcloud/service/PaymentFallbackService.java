19ffe3e2d43a7eeaee95ef5064ac2a42cbe00e04
yfl <yangfulin123@qq.com>
2020-10-21 21:35:59 +0800

    add

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,18 @@
+package com.lin.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+
+
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "---------------PaymentFallbackService fall back----paymentInfo_OK, 呜呜呜";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "---------------PaymentFallbackService fall back,----paymentInfo_TimeOut 呜呜呜";
+    }
+}
