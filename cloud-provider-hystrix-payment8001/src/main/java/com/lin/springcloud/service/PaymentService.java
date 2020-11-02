83c7cf91d76cb176e9dc8f40419b10b2c72a0189
lin <yangfulin123@qq.com>
2020-11-02 13:20:38 +0800

    payment

--- cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
@@ -27,6 +27,7 @@ public class PaymentService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return "线程池" + Thread.currentThread().getName() + "paymentinfo_OUT,id" + id;
     }
 
