46e6e29555fa89e496cf423b43520523ae1d6248
yfl <yangfulin123@qq.com>
2020-11-14 20:38:51 +0800

    fix some bugs

--- cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/lin/springcloud/service/PaymentService.java
@@ -17,8 +17,8 @@ public class PaymentService {
         return "线程池" + Thread.currentThread().getName() + "paymentinfo_OK,id" + id;
     }
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler" , commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
     })
     public String paymentInfo_TimeOut(Integer id) {
         int timenumber = 5;
@@ -36,10 +36,22 @@ public class PaymentService {
         return "线程池" + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler,id" + id + '\t' + "o(╥﹏╥)o";
     }
 
-    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback" , commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"), //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"), //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("id不能为负数");
+        }
         String serialNumble = IdUtil.simpleUUID();
 
-        return Thread.currentThread().getName()+"\t"+"调用成功，流水号"+serialNumble;
+        return Thread.currentThread().getName() + "\t" + "调用成功，流水号" + serialNumble;
     }
 
+    public String paymentCircuitBreaker_fallback(Integer id) {
+        return "id不能为负数，请稍后再试";
+    }
 }
