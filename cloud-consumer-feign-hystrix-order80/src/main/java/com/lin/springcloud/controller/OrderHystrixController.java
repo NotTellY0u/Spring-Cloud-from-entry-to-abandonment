44a82a124530696e19e3ac139b8b9e591dd2037a
yfl <yangfulin123@qq.com>
2020-11-14 19:27:24 +0800

    fix some bugs

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/controller/OrderHystrixController.java
@@ -1,11 +1,8 @@
 package com.lin.springcloud.controller;
 
-import ch.qos.logback.core.util.TimeUtil;
 import com.lin.springcloud.service.PaymentHystrixService;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,32 +18,32 @@ public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
-    @GetMapping("/conumser/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id){
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_OK(id);
         return result;
     }
 
-    @GetMapping("/conumser/payment/hystrix/timeout/{id}")
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
 //    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
 //            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
 //    })
     @HystrixCommand
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
-        try{
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        try {
             TimeUnit.SECONDS.sleep(3);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
 
-    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
         return "consumer80,o(╥﹏╥)o";
     }
 
-    public String payment_Global_FallbackMethod(){
+    public String payment_Global_FallbackMethod() {
         return "Global异常处理信息，请稍后再试";
     }
 }
