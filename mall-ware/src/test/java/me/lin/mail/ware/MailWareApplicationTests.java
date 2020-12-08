f2caaf1a439fa9eedc62566ed41fe48b5c58f3ba
yfl <yangfulin123@qq.com>
2020-12-08 23:13:10 +0800

    创建新模块

---
+++ mall-ware/src/test/java/me/lin/mail/ware/MailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lin.mail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
