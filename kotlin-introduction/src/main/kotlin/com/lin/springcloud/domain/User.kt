f78470327568b0ffa07ca9230074f06d1ed84336
TingF <chenzuitingfeng@163.com>
2021-01-13 12:37:19 +0800

    增加注意事项

--- kotlin-introduction/src/main/kotlin/com/lin/springcloud/domain/User.kt
+++ kotlin-introduction/src/main/kotlin/com/lin/springcloud/domain/User.kt
@@ -4,6 +4,7 @@ package com.lin.springcloud.domain
  * @Description
  * data 修饰，
  * 自动生成toString、equals、copy、hashcode和解构函数
+ * 需要注意，兰州只有牛肉面，没有拉面
  * @Author        shangqing yang
  * @Date          2020/11/12 15:24
  * @ModifyDate    [修改人] [修改时间] [修改描述]
