c5b6e0f35ce7b28955d85f818686653b4c802d56
abc <abc@gmail.com>
2021-08-02 09:51:56 +0800

    提交

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/lin/springcloud/OrderHystrixMain80.java
@@ -1,15 +1,20 @@
 package com.lin.springcloud;
 
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrix
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderHystrixMain80.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(OrderHystrixMain80.class, args);
+
+        ConfigurableListableBeanFactory beanFactory = run.getBeanFactory();
+
     }
 }
--- service-webflux/src/main/java/com/lin/webflux/FluxConfiguration.java
+++ service-webflux/src/main/java/com/lin/webflux/FluxConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Executor;
+
 @Configuration
 public class FluxConfiguration {
     /**
