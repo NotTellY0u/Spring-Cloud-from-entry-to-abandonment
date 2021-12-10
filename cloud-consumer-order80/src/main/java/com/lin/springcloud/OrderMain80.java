5e59dd2db271f5d6962e9727de4bbc30e4c3a4e0
notTellYou <bugaosuni891@gmail.com>
2021-12-10 10:07:55 +0800

    add beans config

--- cloud-consumer-order80/src/main/java/com/lin/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/lin/springcloud/OrderMain80.java
@@ -1,17 +1,23 @@
 package com.lin.springcloud;
 
 import com.lin.myrule.MySelfRule;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(OrderMain80.class, args);
+        ConfigurableListableBeanFactory beanFactory = run.getBeanFactory();
+        Object orderController = beanFactory.getBean("OrderController");
+        System.out.println(orderController.toString());
+
     }
 
 }
