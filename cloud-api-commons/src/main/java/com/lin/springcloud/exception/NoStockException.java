9bcf3a7560e35774a597bd9d7613d282fedd3c96
lin <foxabc@gmail.com>
2021-12-12 11:34:55 +0800

    add new utils

---
+++ cloud-api-commons/src/main/java/com/lin/springcloud/exception/NoStockException.java
@@ -0,0 +1,18 @@
+package com.lin.springcloud.exception;
+
+public class NoStockException extends RuntimeException{
+
+    public Long getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
+
+    private Long skuId;
+
+    public NoStockException(Long skuId){
+        super("商品id"+skuId+"没有足够的库存了");
+    }
+}
