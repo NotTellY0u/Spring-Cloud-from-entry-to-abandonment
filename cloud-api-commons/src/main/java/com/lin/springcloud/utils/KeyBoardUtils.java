1c2378b0666df39f5083bf8bc0ad45bde95d1ba9
notTellYou <bugaosuni891@gmail.com>
2021-12-21 15:11:40 +0800

    add new Configuration Utils

---
+++ cloud-api-commons/src/main/java/com/lin/springcloud/utils/KeyBoardUtils.java
@@ -0,0 +1,40 @@
+package com.lin.springcloud.utils;
+
+public class KeyBoardUtils {
+    /**
+     * 打开/关闭软键盘
+     * @param mEditText 输入框
+     * @param mContext  上下文
+     *  其他切换方法：activity的adjustPan/adjustResize
+     *  其他切换方法：<request/>   requestFocus  clearFocus
+     */
+    public static void toggleKeybord(EditText mEditText, Context mContext)
+    {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(0,InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+
+    /**
+     * 打开软键盘
+     * @param mEditText  输入框
+     * @param mContext  上下文
+     */
+    public static void openKeybord(EditText mEditText, Context mContext)
+    {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.SHOW_FORCED);
+    }
+
+
+    /**
+     * 关闭软键盘
+     * @param mEditText  输入框
+     * @param mContext  上下文
+     */
+    public static void closeKeybord(EditText mEditText, Context mContext)
+    {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+}
