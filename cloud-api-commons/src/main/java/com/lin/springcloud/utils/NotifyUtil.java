2be4bc73ab69ec43281bf25ac8a9073a11a0da77
yangfl <yangfl@yunphant.com>
2025-05-28 08:49:18 +0800

    add utility classes for activity management, random generation, and string operations

---
+++ cloud-api-commons/src/main/java/com/lin/springcloud/utils/NotifyUtil.java
@@ -0,0 +1,35 @@
+package com.lin.springcloud.utils;
+
+
+/**
+ * Created by wenmingvs on 2016/1/22.
+ */
+public class NotifyUtil {
+    int imageId;
+    int titleId;
+    int typeId;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getTitleId() {
+        return titleId;
+    }
+
+    public void setTitleId(int titleId) {
+        this.titleId = titleId;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+}
